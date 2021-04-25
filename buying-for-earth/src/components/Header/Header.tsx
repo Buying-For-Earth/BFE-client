bf6829a7b6976f45a8be0643bf120317372341b7
mjs0818 <tjfaudwo09@gmail.com>
2021-04-25 23:34:14 +0900

    Pending Home view

---
+++ buying-for-earth/src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import './Header.scss';
+
+type HeaderProps = {
+    title: string,
+}
+
+function Header({ title }: HeaderProps) {
+    return <div className='main-header'>
+        {title === 'home' && <div className="title">바잉포어스</div>}
+    </div>
+}
+
+Header.defaultProps = {
+    title: 'home',
+}
+
+export default Header;
--- buying-for-earth/src/pages/Home/Home.tsx
+++ buying-for-earth/src/pages/Home/Home.tsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import './Home.scss'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Header from '../../components/Header'
 
 function Home() {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+  };
+  const images = [img1, img2, img3, img4];
   return (
-  <div>
-    Home
-  </div>
+    <>
+      <Header />
+      <div className='container'>
+        <div className='recommend'>
+          <div className='title'>[MD추천] 이 상품 어때요?</div>
+          <div className='recommend-prod'>
+            <Slider {...settings}>
+              {images.map((img, i) => (
+                <div key={i}>
+                  <img src={img} alt="" />
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </div>
+        <div className='new'>
+          <div className='title'>[New] 새롭게 입점된 제품</div>
+          <div className='new-prod'>
+            {images.map((img, i) => (
+              <div key={i}>
+                <img src={img} alt="" />
+                <div className="item-name">이름</div>
+                <div className="item-price">가격</div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="popular">
+          <div className='title'>[2030女] 많이 찾는 제품</div>
+          <div className='pop-prod'>
+            {images.map((img, i) => (
+              <div key={i}>
+                <img src={img} alt="" />
+                <div className="item-name">이름</div>
+                <div className="item-price">가격</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
