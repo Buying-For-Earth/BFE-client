6e5a0484aec9712b89a136fc1611792b389a324d
JunSeobKim <wnstjq616@gmail.com>
2021-04-25 19:06:18 +0900

    Modify - move component folder

--- buying-for-earth/src/pages/Search/Search.tsx
+++ buying-for-earth/src/pages/Search/Search.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Search.scss';
-import SearchProduct from '../../components/Search/SearchProduct';
-import SearchResult from '../../components/Search/SearchResult';
+import SearchProduct from './components/SearchProduct';
+import SearchResult from './components/SearchResult';
 
 function Search() {
   return (
