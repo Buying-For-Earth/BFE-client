9c04680acbde06d6a3a50512efbbb824030cee57
mjs0818 <tjfaudwo09@gmail.com>
2021-04-26 17:40:28 +0900

    Completed - Home view

--- buying-for-earth/src/pages/Home/Home.tsx
+++ buying-for-earth/src/pages/Home/Home.tsx
@@ -1,71 +1,34 @@
-import img1 from './img1.jpg'
-import img2 from './img2.jpg'
-import img3 from './img3.jpg'
-import img4 from './img4.jpg'
-import './Home.scss'
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Header from '../../components/Header'
+import React from 'react';
+import img1 from './img1.jpg';
+import img2 from './img2.jpg';
+import img3 from './img3.jpg';
+import img4 from './img4.jpg';
+import './Home.scss';
+import Header from '../../components/Header';
+import ItemList from './components/ItemList';
+import Recommend from './components/Recommend';
 
 function Home() {
-  const settings = {
-    infinite: true,
-    speed: 500,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-  };
   const images = [img1, img2, img3, img4];
   return (
     <>
       <Header />
-      <div className='container'>
-        <div className='recommend'>
-          <div className='title'>[MD추천] 이 상품 어때요?</div>
-          <div className='recommend-prod'>
-            <Slider {...settings}>
-              {images.map((img, i) => (
-                <div key={i}>
-                  <img src={img} alt="" />
-                </div>
-              ))}
-            </Slider>
+      <div className="home--container">
+        <div className="home__recommend">
+          <div className="home__recommend__title title">
+            [MD추천] 이 상품 어때요?
           </div>
+          <Recommend images={images} />
         </div>
-        <div className='new'>
-          <div className='title'>[New] 새롭게 입점된 제품</div>
-          <div className='new-prod'>
-            {images.map((img, i) => (
-              <div key={i}>
-                <img src={img} alt="" />
-                <div className="item-name">이름</div>
-                <div className="item-price">가격</div>
-              </div>
-            ))}
-          </div>
+        <div className="home__new">
+          <div className="home__new__title title">[New] 새롭게 입점된 제품</div>
+          <ItemList images={images} />
         </div>
-        <div className="popular">
-          <div className='title'>[2030女] 많이 찾는 제품</div>
-          <div className='pop-prod'>
-            {images.map((img, i) => (
-              <div key={i}>
-                <img src={img} alt="" />
-                <div className="item-name">이름</div>
-                <div className="item-price">가격</div>
-              </div>
-            ))}
+        <div className="home__popular">
+          <div className="home__popular__title title">
+            [2030女] 많이 찾는 제품
           </div>
-        </div>
-      </div>
-      <div>
-        <div>
-          <div>
-
-          </div>
+          <ItemList images={images} />
         </div>
       </div>
     </>
