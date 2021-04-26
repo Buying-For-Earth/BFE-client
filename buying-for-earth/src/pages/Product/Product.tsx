73240c9177bb3ee34a8f989540c239e3198b9671
JunSeobKim <wnstjq616@gmail.com>
2021-04-26 21:30:04 +0900

    Add - ProductBuy

--- buying-for-earth/src/pages/Product/Product.tsx
+++ buying-for-earth/src/pages/Product/Product.tsx
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import ProductDetailNav from "./components/ProductDetailNav";
 import ProductDetail from "./components/ProductDetail";
 import ProductReview from "./components/ProductReview";
+import ProductBuy from "./components/ProductBuy";
 import "./Product.scss";
 
 function Product() {
@@ -13,6 +14,7 @@ function Product() {
         <Route path="/product" exact render={() => <ProductDetail />} />
         <Route path="/product/review" render={() => <ProductReview />} />
       </Switch>
+      <ProductBuy />
     </div>
   );
 }
---
+++ buying-for-earth/src/pages/Product/components/ProductBuy.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ProductBuy.scss";
+
+function ProductBuy() {
+  return (
+    <div className="product-buy--container">
+      <div className="product-buy-btn-set">
+        <div className="product-buy__btn">구매하기</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductBuy;
