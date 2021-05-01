385bdd915c1e1252e3c0ef548b8edcc591f0b477
mjs0818 <tjfaudwo09@gmail.com>
2021-05-01 22:33:58 +0900

    Connet - home-connect-productDetail

--- buying-for-earth/src/pages/Product/components/ProductDetail.tsx
+++ buying-for-earth/src/pages/Product/components/ProductDetail.tsx
@@ -1,13 +1,32 @@
-import React from "react";
-import "./ProductDetail.scss";
+import React, { useEffect, useState } from 'react';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { fakeData } from '../../../fakeData';
+import './ProductDetail.scss';
 
-function ProductDetail() {
+interface MatchParams {
+  id: string;
+}
+
+function ProductDetail({ match }: RouteComponentProps<MatchParams>) {
+  const [item, setItem] = useState({ image: '', name: '', price: 0 });
+
+  useEffect(() => {
+    for (let item of fakeData) {
+      if (item.id === Number(match.params.id)) {
+        setItem({ image: item.image, name: item.itemName, price: item.price });
+        break;
+      }
+    }
+  }, []);
+
   return (
     <div className="product-detail--container">
-      <div className="product-detail__image"></div>
-      <div className="product-detail__name">친환경대나무칫솔</div>
-      <div className="product-detail__discount-price">{"1,100"}원</div>
-      <div className="product-detail__price">{"1,300"}원</div>
+      {/* <div className="product-detail__image"> */}
+      <img className="product-detail__image" src={item.image} alt="" />
+      {/* </div> */}
+      <div className="product-detail__name">{item.name}</div>
+      <div className="product-detail__discount-price">{item.price}원</div>
+      {/* <div className="product-detail__price">{'1,300'}원</div> */}
       <div className="product-detail__image"></div>
       <div className="product-detail__image"></div>
       <div className="product-detail__image"></div>
@@ -15,4 +34,4 @@ function ProductDetail() {
   );
 }
 
-export default ProductDetail;
+export default withRouter(ProductDetail);
