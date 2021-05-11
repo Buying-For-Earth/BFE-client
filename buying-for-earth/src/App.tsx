bb4bf9e83b04852b235a327e9d11a327abda04f7
JunSeobKim <wnstjq616@gmail.com>
2021-05-11 23:04:15 +0900

    Completed - pull upstream dev

--- buying-for-earth/src/App.tsx
+++ buying-for-earth/src/App.tsx
@@ -10,6 +10,7 @@ import Landing from './pages/Landing';
 import Navigation from './components/Navigation';
 import Info from './pages/Info';
 import Header from './components/Header';
+import Complete from './pages/Complete';
 
 function App() {
   const location = useLocation();
@@ -19,7 +20,6 @@ function App() {
   }
 
   return (
-
     <div className="wrap">
       <div className="wrap-webDiv">
         <div className="wrap-webDiv__text">
